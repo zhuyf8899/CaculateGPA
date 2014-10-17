bec44033d79b0b4b2ca1305f03ebc3ab61b788d3
ZhuYifan <zhuyf8899@163.com>
2014-10-17 15:16:59 +0800

    first edition
    For test only

---
+++ CaculateGPA/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace CaculateGPA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewTable = new System.Windows.Forms.DataGridView();
+            this.buttonCaluate = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTable
+            // 
+            this.dataGridViewTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTable.Location = new System.Drawing.Point(35, 73);
+            this.dataGridViewTable.Name = "dataGridViewTable";
+            this.dataGridViewTable.RowTemplate.Height = 23;
+            this.dataGridViewTable.Size = new System.Drawing.Size(653, 265);
+            this.dataGridViewTable.TabIndex = 2;
+            // 
+            // buttonCaluate
+            // 
+            this.buttonCaluate.Location = new System.Drawing.Point(319, 38);
+            this.buttonCaluate.Name = "buttonCaluate";
+            this.buttonCaluate.Size = new System.Drawing.Size(86, 23);
+            this.buttonCaluate.TabIndex = 3;
+            this.buttonCaluate.Text = "计算绩点";
+            this.buttonCaluate.UseVisualStyleBackColor = true;
+            this.buttonCaluate.Click += new System.EventHandler(this.buttonCaluate_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelResult.Location = new System.Drawing.Point(904, 28);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(110, 31);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "使用方法";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(732, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 92);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "    首先选择打开文件导入.xls的Excel文件，要表格一定包含课程名称，课程性质，成绩，补考成绩，重修成绩，学分这几列（要求列名和刚才说名称对上不然无法识别" +
+    "）。之后点击计算绩点便得出结果。注意：如果成绩为非数字，请在导入之前或之后手动修改一下分数使之为一个数字。";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(652, 346);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(380, 17);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "绩点计算插件（壮哉我大北信科坑爹的绩点算法专用版） ©  凡爷制作";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(142, 38);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(89, 23);
+            this.buttonOpen.TabIndex = 10;
+            this.buttonOpen.Text = "打开文件...";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "首先";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "然后";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(732, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(298, 103);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "    内置不及格时将补考和重修成绩纳入考虑，若补考或重修及格绩点则为1，若均为不及格则此时该门课成绩绩点为0，且计算平均绩点是加入加权中计算。注：北信科绩点计算" +
+    "方式为60-69为1绩点，70-79为2绩点，以此类推，最高绩点为4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 372);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCaluate);
+            this.Controls.Add(this.dataGridViewTable);
+            this.Name = "Form1";
+            this.Text = "GPA计算";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTable;
+        private System.Windows.Forms.Button buttonCaluate;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CaculateGPA/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Data.OleDb;
+
+namespace CaculateGPA
+{
+    public partial class Form1 : Form
+    {
+        public DataTable dt_operate;
+        string textBoxOpenPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCaluate_Click(object sender, EventArgs e)
+        {
+            DataTable dt_dest;
+            double sumGPA = 0.0;
+            double sumCredit = 0.0;
+            int count = 0;
+            try
+            {
+                dt_dest = new DataTable();
+                dt_dest = dt_operate;
+                while (!dt_dest.Rows[count][1].Equals("")) 
+                {
+                    double tempScore;
+                    double tempGPA;
+                    tempScore = Convert.ToDouble(dt_dest.Rows[count][2]);
+                    switch ((int)(tempScore / 10))
+                    {
+                        case 10:
+                            tempGPA = 4;
+                            break;
+                        case 9:
+                            tempGPA = 4;
+                            break;
+                        case 8:
+                            tempGPA = 3;
+                            break;
+                        case 7:
+                            tempGPA = 2;
+                            break;
+                        case 6:
+                            tempGPA = 1;
+                            break;
+                        default:
+                            tempGPA = 0;
+                            break;
+                    }
+                    if (tempGPA == 0)
+                    {
+                        double Scorebukao = 0.0;
+                        double Scorechongxiu = 0.0;
+                        if (!(dt_dest.Rows[count][4].ToString().Equals(""))) 
+                        {
+                            Scorebukao=(Convert.ToDouble(dt_dest.Rows[count][4].ToString()));
+                        }
+                        if (!(dt_dest.Rows[count][5].ToString().Equals(""))) 
+                        {
+                            Scorechongxiu=(Convert.ToDouble(dt_dest.Rows[count][5].ToString()));
+                        }
+
+                        if ( (Scorebukao>= 60) || (Scorechongxiu >= 60))
+                            tempGPA = 1;
+                        else
+                            tempGPA = 0;
+                    }
+                    sumCredit += Convert.ToDouble(dt_dest.Rows[count][3].ToString());
+                    sumGPA += (tempGPA * Convert.ToDouble(dt_dest.Rows[count][3].ToString()));
+                    count++;
+                }
+                
+            }
+            catch (Exception err)
+            {
+                ;//MessageBox.Show(err.Message);
+            }
+            finally 
+            {
+                double res = sumGPA / sumCredit;
+                MessageBox.Show("本次共统计科目"+(count)+"门，\n"+"计算总学分为："+ sumCredit.ToString()+"\n总绩点学分和为："+sumGPA.ToString()+"\n平均绩点为："+ res.ToString());
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            AboutBox1 ab = new AboutBox1();
+            ab.Show();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            //选择打开文件路径触发的事件
+            try
+            {
+                //打开文件对话框
+                OpenFileDialog ofd = new OpenFileDialog();
+                //默认路径
+                ofd.InitialDirectory = @"D:\学习资料\windows程序设计\CaculateGPA";
+                //筛选类型
+                ofd.Filter = "Excel2003-2007 files|*.xls";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //从这个字符串获取打开内容
+                    textBoxOpenPath = ofd.FileName;
+                    //构造子字符串
+                    string constr = "Provider = MicroSoft.Jet.OLEDB.4.0;" + "Data Source=" + textBoxOpenPath + ";Extended Properties = \"Excel 8.0;HDR=Yes;\";";
+                    //实例化对象con打开数据源，源地址为变量textOpenPath的内容
+                    OleDbConnection con = new OleDbConnection(constr);
+                    con.Open();
+                    //初始化表格
+                   // DataTable dt = new DataTable();
+                    //获取表格文件中的表
+                    //dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                    //实例化适配器对象oda，并且向源中发送SQL语句
+                    OleDbDataAdapter oda = new OleDbDataAdapter("Select 课程名称,课程性质,成绩,学分,补考成绩,重修成绩 From [Sheet1$] WHERE 课程性质='公共基础课' OR 课程性质='专业基础课' OR 课程性质='专业课' OR 课程性质='必修课'", con);
+                    //实例化内存中表格对象dtSource
+                    dt_operate = new DataTable();
+                    //适配器对象oda使用Fill方法填充内存中表格的dtSource
+                    oda.Fill(dt_operate);
+                    //将表单中dataSourceInfo源连接至dtSource
+                    dataGridViewTable.DataSource = dt_operate;
+                }
+            }
+            catch (Exception err)
+            {
+                //提供调试信息
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
